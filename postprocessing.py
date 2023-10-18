b907139053b813c48061619b7efeb094836b8caa
brandendupont-mcw <bdupont@mcw.edu>
2023-10-17 22:06:08 -0400

    send first git scrape push

--- postprocessing.py
+++ postprocessing.py
@@ -7,14 +7,99 @@ import numpy as np
 
 if __name__ == "__main__":
 
+    # import data
+    PAST_JAIL_DATA = pd.read_csv("data/cook-jail-data.csv")
+    FAILED_URL = pd.read_csv("data/failed_url.csv")
+
   
-    df = pd.DataFrame(np.random.randint(
-    0, 100, size=(10, 4)), columns=list('ABCD'))
+    today_yr = date.today().year
+    today_month =  date.today().month
+    today_day =  date.today().day 
 
-    df.to_csv("df_output.csv")
+    # add leading zeros to single digit month and days to match the sherrif's pdf template
+    month = "{:02d}".format(today_month)
+    day = "{:02d}".format(today_day)
 
-    df = camelot.read_pdf('https://www.cookcountysheriffil.gov/wp-content/uploads/2020/05/CCSO_BIU_CommunicationsCCDOC_v1_2020_05_29.pdf')
-    if len(df) ==1:
-        dfs = df[0].df
+    # create the templated pdf
+    pdf_url = f"https://www.cookcountysheriffil.gov/wp-content/uploads/{today_yr}/{month}/CCSO_BIU_CommunicationsCCDOC_v1_{today_yr}_{month}_{day}.pdf"
 
-    dfs.to_csv('parsed_pdf_test.csv')
+    # parse the pdf
+    try:
+        #parse the pdf table
+        df = camelot.read_pdf(pdf_url)
+        pdf_date = str.replace(str(pdf_url).split(".")[-2].split("/")[-1].split("v1_")[-1], "_", "-")
+        
+        if len(df) == 1:
+            dfs = df[0].df
+
+            dfs.to_csv(f'data/{pdf_date}-parsed-pdf-table.csv', index=False)
+
+        else:
+            print("PDF Report Name: ", pdf_report_name, " had more than one table")
+            url_failed = True
+            print("unsuccessful:",pdf_url)
+
+    except:
+        url_failed = True
+        print("unsuccessful:",pdf_url)
+
+    # replace any null values
+    dfs = dfs.replace('', np.nan)
+    # remove any completely null rows
+    dfs = dfs.dropna(axis=1, how='all')
+
+    # split any tables with the table in one row seperated by a /n character
+    if dfs.shape[1] < 2:
+        dfs[[0,1]] = dfs[0].str.split('\n', expand=True)
+        dfs = dfs.replace('', np.nan)
+        dfs = dfs.dropna(axis=1, how='all')
+
+    dfs[0] = dfs[0].str.strip().str.lower().str.replace(" ","-")
+
+    #subset to only include these values
+    dfs = dfs[dfs[0].isin(['total-male-and-female',
+        'jail-population', 'community-corrections'])]
+
+
+    dfs['Date'] = pdf_date
+
+    # make sure the dataframe is the correct size before appending
+    if dfs.shape == (3, 3):
+        
+        dfs[0] = dfs[0].str.strip().str.title().str.replace("-"," ")
+        #pivot data horizontally
+        pdf_table_final = dfs.pivot(index='Date', columns=0, values=1).reset_index()
+        
+        # remove any commas in the table
+        pdf_table_final[['Community Corrections', 'Jail Population',
+       'Total Male And Female']] = pdf_table_final[['Community Corrections', 'Jail Population',
+       'Total Male And Female']].apply(lambda x: x.str.replace(",",""))
+        
+        print(pdf_table_final.head())
+        
+        combine = pd.concat([PAST_JAIL_DATA, pdf_table_final])
+        
+        #ensure types are numeric
+        combine[['Jail Population', 'Community Corrections',
+       'Total Male And Female']].apply(lambda x: pd.to_numeric(x))
+        
+        # ensure dates are dates
+        combine['Date'] = pd.to_datetime(combine['Date'])
+
+        #deduplicate
+        combine = combine.groupby('Date').first().reset_index()
+
+        #overwrite and save data
+        combine.to_csv('data/cook-jail-data.csv', index=False)
+
+# save failed url if the pdf broke
+if url_failed == True:
+
+    # add the failed dataset to pandas
+    FAILED_URL['failed_url'] = FAILED_URL['failed_url'].add(pdf_url)
+
+    #overwrite and save data
+    FAILED_URL.to_csv('data/failed_url.csv', index=False)
+
+
+

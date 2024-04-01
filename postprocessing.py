144414a1e4c552793e38c1fa6d31e8828325df5f
brandendupont-mcw <bdupont@mcw.edu>
2024-03-31 23:15:57 -0400

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -27,8 +27,8 @@ def add_chart_calculation():
 
   em_up_or_down = np.where(pfa_diff['Community Corrections'].values[0] < 0, 'down', 'up')
 
-  chart_calculation_string = f"<br>On {calendar.month_name[jpop['Date'].max().month]} {str(jpop['Date'].max().day) + get_ordinal_suffix(jpop['Date'].max().day)} the Cook County average daily jail population is {up_or_down} {HTML_STRING}{pfa_change['Jail Population'].values[0].round(3) * 100}%</b> — a difference in ADP of {HTML_STRING}{pfa_diff['Jail Population'].values[0]}</b> since the implementation of the PFA on September 17th."
-  em_chart_calculation_string = f"<br>On {calendar.month_name[jpop['Date'].max().month]} {str(jpop['Date'].max().day) + get_ordinal_suffix(jpop['Date'].max().day)} the Cook County average daily electronic monitoring population is {em_up_or_down} {HTML_STRING}{pfa_change['Community Corrections'].values[0].round(3) * 100}%</b> — a difference in individuals on EM of {HTML_STRING}{pfa_diff['Community Corrections'].values[0]}</b> since the implementation of the PFA on September 17th."
+  chart_calculation_string = f"<br>On {calendar.month_name[jpop['Date'].max().month]} {str(jpop['Date'].max().day) + get_ordinal_suffix(jpop['Date'].max().day)} the Cook County average daily jail population is {up_or_down} {HTML_STRING}{pfa_change['Jail Population'].round(3).values[0] * 100}%</b> — a difference in ADP of {HTML_STRING}{pfa_diff['Jail Population'].values[0]}</b> since the implementation of the PFA on September 17th."
+  em_chart_calculation_string = f"<br>On {calendar.month_name[jpop['Date'].max().month]} {str(jpop['Date'].max().day) + get_ordinal_suffix(jpop['Date'].max().day)} the Cook County average daily electronic monitoring population is {em_up_or_down} {HTML_STRING}{pfa_change['Community Corrections'].round(3).values[0] * 100}%</b> — a difference in individuals on EM of {HTML_STRING}{pfa_diff['Community Corrections'].values[0]}</b> since the implementation of the PFA on September 17th."
   API_KEY = os.environ['DATAWRAPPER_API']
   dw = Datawrapper(access_token=API_KEY)
   dw.update_description(chart_id='JoeoH', intro=chart_calculation_string)
